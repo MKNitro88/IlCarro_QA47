911c11639c1031545efd3032ee6caf720acd3077
MKNitro88 <mati88kaplan@outlook.com>
2025-05-25 21:08:01 +0300

    new restructured project

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,20 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+
+    @Test
+    public void loginPositiveTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.fillLoginForm("test_mkii@gmail.com","@Password123");
+        loginPage.clickBtnLogin();
+
+
+    }
+}
